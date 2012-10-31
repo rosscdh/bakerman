ff077e0c3d7cb057ec1f703985002841a1334b80
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-10-31 15:24:07 +0100

    Added template tags

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ bakerman
 ========
 
 Django-CMS static site generator
+
+A simple app to print out a static version of a django-cms site
---
+++ templatetags/landingpages_tags.py
@@ -0,0 +1,20 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('js/page_language_urls.html')
+def document_categories_home(page):
+	language_list = []
+	if page:
+		for lang in page.get_languages():
+			short_lang, locale = lang.split('-')
+			language_list.append({
+				'lang': lang,
+				'short_lang': short_lang,
+				'url': page.get_absolute_url(language=lang)
+			})
+
+	return {
+		'language_list': language_list
+	}
